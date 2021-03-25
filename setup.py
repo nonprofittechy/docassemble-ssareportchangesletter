0aa0ac12880c0fa8ce8f2914209014a0e4c6c79a
System Administrator <qsteenhuis@gmail.com>
2021-03-25 16:52:42 -0400

    Add branding and save interview stats

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ssareportchangesletter',
-      version='0.0.20',
+      version='0.0.21',
       description=('An interview to help an SSI or SSDI recipient report changes to the Social Security Administration'),
       long_description='# docassemble.ssareportchangesletter\r\n\r\nAn interview to help an SSI or SSDI recipient report changes to the Social Security Administration\r\n\r\n## Author\r\n\r\nQuinten Steenhuis, qsteenhuis@gmail.com\r\n\r\n## Changelog\r\n* 2020-12-31 Migrate to asking for details one at a time; more logical detail questions\r\n* 2020-12-04 Upgrade for newest docassemble version\r\n* 2019-09-21 Cleanup / integrate ssn + tel auto-input validation\r\n* 2019-08-26 Add phone number auto-format, complete template variable mapping\r\n* 2019-08-20 Reorganize question order to be more logical, add review/edit ledgers, separate deposits and withdrawals\r\n* 2019-08-18 Add section headings to record of dedicated account\r\n* 2019-03-23 Include the Record of dedicated account\r\n* 2019-03-06 Work on question order and commpleting letter\r\n* 2019-02-26 Use SSA API to list nearby offices',
       long_description_content_type='text/markdown',
@@ -53,7 +53,7 @@ setup(name='docassemble.ssareportchangesletter',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.income>=0.0.33'],
+      install_requires=['docassemble.income>=0.0.36'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ssareportchangesletter/', package='docassemble.ssareportchangesletter'),
      )
